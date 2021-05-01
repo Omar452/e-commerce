b44f6108fbfa6b831383353049d06453f95d8f11
Omar <toumiomar452@gmail.com>
2021-05-01 07:02:22 +0100

    set relationship between categories and items

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Item;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
     use HasFactory;
 
     private $fillable = [ 'name', 'slug'];
+
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
 }
