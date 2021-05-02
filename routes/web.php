b92e2a3ed09df06f012f7e7192f096b0a5d941fb
Rhys Evans <rhys.evans@redditchconnected.co.uk>
2021-05-02 22:29:45 +0100

    code improvements and added a ItemRequest for validation

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
-use App\Http\Middleware\AdminMiddleware;
 
 /*
 |--------------------------------------------------------------------------
