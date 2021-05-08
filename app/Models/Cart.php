8f94f277a00e3366db9816c9757518702ac52632
Omar <toumiomar452@gmail.com>
2021-05-08 15:42:54 +0100

    set user and cart models relationships

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['items','total_items','total_amount','user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
